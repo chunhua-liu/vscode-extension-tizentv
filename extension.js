3405cb995a12ba1c9b1b79f95d25f118ed1318db
tianhao-srcn <tianhao.ni@samsung.com>
2020-05-28 15:40:48 +0800

    update for release test

--- extension.js
+++ extension.js
@@ -33,6 +33,26 @@ function activate(context) {
         vscode.commands.registerCommand('tizentv.witsWatch', async () => launchWits('w'))
     );
 
+    let platform = process.platform;
+    if (platform != 'win32') {
+        const fs = require('fs');
+        let sdbTool = `${__dirname}/tools/sdb/${platform == 'linux'?'linux':'mac'}/sdb`;
+        let secretTool = platform == 'linux' ? `${__dirname}/tools/certificate-encryptor/secret-tool` : null;
+        if (platform == 'linux' || platform == 'darwin') {
+            try {
+                fs.accessSync(sdbTool, fs.constants.S_IXUSR);
+            } catch(err) {
+                fs.chmodSync(sdbTool, fs.constants.S_IXUSR)
+            }
+        } 
+        if (platform == 'linux') {
+            try {
+                fs.accessSync(secretTool, fs.constants.S_IXUSR);
+            } catch(err) {
+                fs.chmodSync(secretTool, fs.constants.S_IXUSR)
+            }
+        }
+    }
 }
 exports.activate = activate;
 
